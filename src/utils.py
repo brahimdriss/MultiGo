38f6ad3b0f40d77670b277021b99ded2147dfa27
Brahim <brahimdriss.bd@gmail.com>
2022-04-15 13:09:35 +0200

    Added fast board

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 from MultiGo.src import gotypes
+import numpy as np
 
 COLS = 'ABCDEFGHJKLMNOPQRST'
 STONE_TO_CHAR = {
@@ -27,3 +28,19 @@ def print_board(board):
             line.append(STONE_TO_CHAR[stone])
         print('%s%d %s' % (bump, row, ''.join(line)))
     print('    ' + '  '.join(COLS[:board.num_cols]))
+
+class MoveAge():
+    def __init__(self, board):
+        self.move_ages = - np.ones((board.num_rows, board.num_cols))
+
+    def get(self, row, col):
+        return self.move_ages[row, col]
+
+    def reset_age(self, point):
+        self.move_ages[point.row - 1, point.col - 1] = -1
+
+    def add(self, point):
+        self.move_ages[point.row - 1, point.col - 1] = 0
+
+    def increment_all(self):
+        self.move_ages[self.move_ages > -1] += 1
